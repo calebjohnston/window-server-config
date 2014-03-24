ead1adb2fa8908f04af6bc869cc37444be9f6b6f
Caleb Johnston <caleb.johnston@gmail.com>
2014-03-23 23:20:48 -0400

    Fixed some formatting issues.

--- WindowServerConfig/DisplayDevice.mm
+++ WindowServerConfig/DisplayDevice.mm
@@ -53,24 +53,11 @@ DisplayMode::~DisplayMode()
 
 std::string DisplayMode::toString() const
 {
-	/*
-	NSString* fmt = @"    - Display Mode\n\t %@ %li\n\t %@ %.1f\n\t %@ %i\n\t %@ %i\n\t %@ (%i x %i)\n\t %@ %@\n";
-	NSString* output = [NSString stringWithFormat:fmt,
-						@"Device type:\t", type,
-						@"Refresh Rate:\t", refreshRate,
-						@"IO Flags:\t", ioFlags,
-						@"IO ModeID:\t", ioModeId,
-						@"Display Size:\t", (uint32_t)width, (uint32_t)height,
-						@"Desktop GUI:\t", (usableForDesktopGui ? @"True" : @"False")];
-	
-	return [output UTF8String];
-	*/
-	
 	std::stringstream output_str;
-	output_str << "    - Display Mode" << std::endl;
+	output_str << "      *\tDisplay Mode" << std::endl << "\t";
 	output_str << "Device type:\t" << type << std::endl << "\t";
 	output_str << "Refresh Rate:\t" << refreshRate << std::endl << "\t";
-	output_str << "IO Flags" << ioFlags << std::endl << "\t";
+	output_str << "IO Flags:\t" << ioFlags << std::endl << "\t";
 	output_str << "IO ModeID:\t" << ioModeId << std::endl << "\t";
 	output_str << "Display Size:\t" << (uint32_t)width << " x " << (uint32_t)height << std::endl << "\t";
 	output_str << "Desktop GUI:\t" << (usableForDesktopGui ? "True" : "False") << std::endl;
@@ -112,36 +99,13 @@ DisplayDevice::~DisplayDevice()
 
 std::string DisplayDevice::toString() const
 {
-/*
-	NSString* fmt = @"%@ - %i\n\t %@ %i\n\t %@ %i\n\t %@ %i\n\t %@ %i\n\t %@ (%.0fmm x %.0fmm)\n\t %@ %.1f\n\t %@ %@\n\t %@ %@\n\t %@ %@\n\t %@ %@\n\t %@ %@\n\t %@ [%.0f,%.0f %.0fx%.0f]\n";
-	NSString* displayName = [self displayNameFromDisplayId:deviceId];
-	NSString* output = [NSString stringWithFormat:fmt,
-						displayName, deviceId,
-						@"Model Number:\t", modelNumber,
-						@"Serial Number:\t", serialNumber,
-						@"Unit Number:\t", unitNumber,
-						@"Vendor Number:\t", vendorNumber,
-						@"Screen Size:\t", screenSize.width, screenSize.height,
-						@"Rotation:\t", rotation,
-						@"Is Active:\t", (isActive ? @"True" : @"False"),
-						@"Is Online:\t", (isOnline ? @"True" : @"False"),
-						@"Is Built-in:\t", (isBuiltin ? @"True" : @"False"),
-						@"Is Primary:\t", (isMain ? @"True" : @"False"),
-						@"HW Accelerated:", (usesOpenGLAcceleration ? @"True" : @"False"),
-						@"Bounds:\t", displayBoundary.origin.x, displayBoundary.origin.y, displayBoundary.size.width, displayBoundary.size.height ];
-//	output = [output stringByAppendingString:[displayMode toNSString]];
- 
-	return [output UTF8String];
-*/
-	
-	
 	std::stringstream output_str;
 	output_str << displayName() << " - " << deviceId << std::endl << "\t";
 	output_str << "Model Number:\t" << modelNumber << std::endl << "\t";
 	output_str << "Serial Number:\t" << serialNumber << std::endl << "\t";
 	output_str << "Unit Number:\t" << unitNumber << std::endl << "\t";
 	output_str << "Vendor Number:\t" << vendorNumber << std::endl << "\t";
-	output_str << "Screen Size:\t (" << screenSize.width << "mm x " << screenSize.height << "mm)" << std::endl << "\t";
+	output_str << "Screen Size:\t(" << screenSize.width << "mm x " << screenSize.height << "mm)" << std::endl << "\t";
 	output_str << "Rotation:\t" << rotation << std::endl << "\t";
 	output_str << "Is Active:\t" << (isActive ? "True" : "False") << std::endl << "\t";
 	output_str << "Is Online:\t" << (isOnline ? "True" : "False") << std::endl << "\t";
@@ -149,7 +113,7 @@ std::string DisplayDevice::toString() const
 	output_str << "Is Primary:\t" << (isMain ? "True" : "False") << std::endl << "\t";
 	output_str << "HW Accelerated:\t" << (usesOpenGLAcceleration ? "True" : "False") << std::endl << "\t";
 	output_str << "Bounds:\t [" << displayBoundary.origin.x << "," << displayBoundary.origin.y << " ";
-	output_str << displayBoundary.size.width << "x" << displayBoundary.size.height << std::endl << "] \t";
+	output_str << displayBoundary.size.width << "x" << displayBoundary.size.height << "] \t" << std::endl;
 	output_str << (displayMode ? displayMode->toString() : "");
 	
 	return output_str.str();
